7a682d72dbd9e082c7f5bd5b22b4b2510a6dd1d0
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-27 19:05:58 +0600

    booking and manageservices added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -9,7 +9,7 @@ const AddService = () => {
     const onSubmit = data => {
         console.log(data);
 
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://mysterious-falls-90974.herokuapp.com/services', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('added successfully');
